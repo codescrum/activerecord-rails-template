4012aca0c9377d302a171efae8005b9199ccbfbb
Juan Pablo Amaya <jp.amaya@codescrum.com>
2015-11-18 18:13:43 -0500

    Change application.rb to load with activerecord

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
+require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_record/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
@@ -26,6 +28,9 @@ module RailsTemplate
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
 
     # Auto-loading lib files
     config.autoload_paths << Rails.root.join('lib/sassish')
