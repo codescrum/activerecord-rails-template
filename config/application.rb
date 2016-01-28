2a4ab46461a1b2fd71ebbe007f3d5eccb78ce62a
Johan Tique <johan.tique@analisistem.com>
2016-01-28 14:45:57 -0500

    deal with heroku rules

--- config/application.rb
+++ config/application.rb
@@ -43,21 +43,3 @@ end
 
 # Require Sassish
 require File.expand_path(File.join('..', '..', 'lib', 'sassish', 'sassish.rb'), __FILE__)
-
-###### Heroku deployment hack ######
-# # copy database config example file into 'real' file
-# if Rails.env.production? || Rails.env.staging?
-#   config_root = File.join(Rails.root, 'config')
-#   # PostgreSQL Config File
-#   postgresql_example_path = File.join(config_root, 'database.yml.example_postgresql')
-#   postgresql_real_path = File.join(config_root, 'database.yml')
-#   `cp #{postgresql_example_path} #{postgresql_real_path}`
-#   # # MySQL Config File
-#   # mysql_example_path = File.join(config_root, 'database.yml.example_mysql')
-#   # mysql_real_path = File.join(config_root, 'database.yml')
-#   # `cp #{mysql_example_path} #{mysql_real_path}`
-#   # Secrets File
-#   secrets_example_path = File.join(config_root, 'secrets.yml.example')
-#   secrets_real_path = File.join(config_root, 'secrets.yml')
-#   `cp #{secrets_example_path} #{secrets_real_path}`
-# end
