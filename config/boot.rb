e48c70fc4801de7c54dabec21c77f44d9bac4616
Miguel Diaz <gato-omega@users.noreply.github.com>
2016-06-11 15:16:40 -0500

    Update boot.rb to account for production deployment by default as well

--- config/boot.rb
+++ config/boot.rb
@@ -5,17 +5,20 @@ require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
 
 ###### Heroku deployment hack ######
 # # copy database config example file into 'real' file
-# if ENV['RACK_ENV'] == 'staging'
+# if ENV['RACK_ENV'] == 'staging' || ENV['RACK_ENV'] == 'production'
 #   config_root = File.join(Bundler.root, 'config')
+
 #   # PostgreSQL Config File
 #   postgresql_example_path = File.join(config_root, 'database.yml.example_postgresql')
 #   postgresql_real_path = File.join(config_root, 'database.yml')
 #   `cp #{postgresql_example_path} #{postgresql_real_path}`
-#   # MySQL Config File
-#   mysql_example_path = File.join(config_root, 'database.yml.example_mysql')
-#   mysql_real_path = File.join(config_root, 'database.yml')
-#   `cp #{mysql_example_path} #{mysql_real_path}`
-#   # Secrets File
+
+#   # MySQL Config File (if you are using mysql for some reason)
+#   # mysql_example_path = File.join(config_root, 'database.yml.example_mysql')
+#   # mysql_real_path = File.join(config_root, 'database.yml')
+#   # `cp #{mysql_example_path} #{mysql_real_path}`
+
+#   # Secrets File (needed)
 #   secrets_example_path = File.join(config_root, 'secrets.yml.example')
 #   secrets_real_path = File.join(config_root, 'secrets.yml')
 #   `cp #{secrets_example_path} #{secrets_real_path}`
